5e20aa7bd6630c95139651b4f3e86691b5283482
jeremycohn <jeremycohn@localhost>
2023-09-06 15:27:22 -0500

    Donezo

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,17 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Insert year you want to check: ");
+		int x = in.nextInt();
+		boolean isLeapYear = false;
+		isLeapYear = ((x % 4 == 0) && (x % 100 != 0)) || (x % 400 == 0);
+		System.out.println(x + " is a lear year: " + isLeapYear);
+	}
+
+}
